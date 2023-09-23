22dc894fe207546bff8c5be353ddfb3bdf6244a8
xV00D00x-og <jamontelee6@gmail.com>
2023-09-23 00:32:12 -0600

    first commit

---
+++ Band Name Generator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print("Welcome to the Band Name Generator!!!")
+
+cityName = input("What's the name of the city you grew up in? ")
+petName = input("What's the name of your pet? ")
+
+print("The name of your band could be:", cityName, petName)
---
+++ README.md
@@ -0,0 +1,2 @@
+# xV00d00x-Projects
+I'm learning Python and this is just a location to throw projects as I finish them. I'm open to feedback, just bear in mind, I'm very new!!!
