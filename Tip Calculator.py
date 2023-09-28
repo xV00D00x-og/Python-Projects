612404da599d4550ecf9130c1b8b118b70bb1ab8
xV00D00x-og <jamontelee6@gmail.com>
2023-09-28 00:36:20 -0600

    Create Tip Calculator.py

---
+++ Tip Calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 3
+# A Simple tip calculator that rounds to two decimal places. Created by Jamonte R. Lee
+
+print("Welcome to the tip calculator.")
+total = (float(input("What was the total bill? ")))
+tip = (float(input("What percentage tip would you like to give? 10, 12, 15, etc? ")))
+people_count = (int(input("How many people will split the bill? ")))
+tip_percentage = float(tip / 100 * total)
+total_calculation = (total + tip_percentage) / people_count
+pay = round(total_calculation, 2)
+if people_count == 1:
+    print("You should pay: ", pay)
+else:
+    print("Each person should pay: ", pay)
